46d57202be31fca7d4dbd0b4ae30be65312ecbf7
unknown <sakalley.yash@gmail.com>
2023-12-26 00:28:49 +0530

    added news api

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ app.post('/api', async (req, res) => {
   }
 });
 
+app.get('/api/news', async (req, res) => {
+  try {
+    const newsApiKey = process.env.NEWS_API_KEY;
+    const { q, from } = req.query;
+    const apiResponse = await axios.get(`https://newsapi.org/v2/everything?q=${q}&from=${from}&sortBy=publishedAt&apiKey=${newsApiKey}`);
+    res.json(apiResponse.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
